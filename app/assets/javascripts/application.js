a4908a728f9efb477c0ef1b47bb4d53ceb8bc757
puiyoke <puiyoke8888@hotmail.com>
2018-07-06 10:03:37 +0800

    robot cannot walk into obstacle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,29 +24,54 @@ var coordinates = function(element) {
 
 $(document).ready(function() {
     $('#east').click(function() {
-        $('#robot').animate({marginLeft:'+=30px'});
-        coordinates();
+        $('#robot').animate({left:'+=30px'});
+        var Y = $("#robot").offset().top;
+        var X = $("#robot").offset().left;
+        $('#coord').text('X: ' + X + ' ' + 'Y: ' + Y);
+        if (Y >= 5 && Y<= 170 && X<=280 && X >=120){
+            $('#robot').animate({left:'-=40px'});
+            $('#coord').text('X: ' + X + ' ' + 'Y: ' + Y);
+        }
+        
     });
     $('#west').click(function() {
-        $('#robot').animate({marginLeft:'-=30px'});
-        coordinates();
+        $('#robot').animate({left:'-=30px'});
+        var Y = $("#robot").offset().top;
+        var X = $("#robot").offset().left;
+        $('#coord').text('X: ' + X + ' ' + 'Y: ' + Y);
+        if (Y >= 5 && Y<= 170 && X<=300 && X >=170){
+            $('#robot').animate({left:'+=40px'});
+            $('#coord').text('X: ' + X + ' ' + 'Y: ' + Y);
+        }
     });
     $('#south').click(function() {
         $('#robot').animate({top:'+=30px'});
-        coordinates();
+        var Y = $("#robot").offset().top;
+        var X = $("#robot").offset().left;
+        $('#coord').text('X: ' + X + ' ' + 'Y: ' + Y);
+        if (Y >= -30 && Y<= 140 && X<=270 && X >=160){
+            $('#robot').animate({top:'-=40px'});
+            $('#coord').text('X: ' + X + ' ' + 'Y: ' + Y);
+        }
     });
     $('#north').click(function() {
         $('#robot').animate({top:'-=30px'});
-        coordinates();
+        var Y = $("#robot").offset().top;
+        var X = $("#robot").offset().left;
+        $('#coord').text('X: ' + X + ' ' + 'Y: ' + Y);
+        if (Y >= 10 && Y<= 205 && X<=280 && X >=140){
+            $('#robot').animate({top:'+=40px'});
+            $('#coord').text('X: ' + X + ' ' + 'Y: ' + Y);
+        }
     });
 
     //drag robot
     $('#robot').draggable({
         start: function() {
-            coordinates('#robot');
+            coordinates();
         },
         stop: function() {
-            coordinates('#robot');
+            coordinates();
         }
     });
 
